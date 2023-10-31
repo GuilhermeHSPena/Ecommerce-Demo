b5954b0eca8106589ae1d93500276f8547aa89a3
Guilherme Pena <guilhermehspena@gmail.com>
2023-10-31 20:54:33 -0300

    first commit

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
+import { loadStripe } from '@stripe/stripe-js';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+})
+export class CartComponent implements OnInit, OnDestroy {
+  cart: Cart = { items: [] };
+  displayedColumns: string[] = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+  dataSource: CartItem[] = [];
+  cartSubscription: Subscription | undefined;
+
+  constructor(private cartService: CartService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cartSubscription = this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = _cart.items;
+    });
+  }
+
+  getTotal(items: CartItem[]): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+
+
+  onCheckout(): void {
+    this.http
+      .post('http://localhost:4242/checkout', {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe('your stripe token');
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
+  }
+}
